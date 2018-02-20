bf15643ae3c954bfffb20a8cd84e6a262b08c8c6
Julian Heng <julianhengwl@gmail.com>
2018-02-20 19:15:49 +0800

    Tabs -> Spaces

--- clock.js
+++ clock.js
@@ -1,16 +1,16 @@
 function startTime() {
-	var date = new Date();
-	var h = date.getHours();
-	var m = date.getMinutes();
-	// add a zero in front of numbers<10
-	m = checkTime(m);
-	document.getElementById("clock").innerHTML = h + ":" + m;
-	t = setTimeout("startTime()",500);
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    document.getElementById("clock").innerHTML = h + ":" + m;
+    t = setTimeout("startTime()",500);
 }
 
 function checkTime(i) {
-	if (i<10) {
-		i = "0" + i;
-	}
-	return i;
+    if (i<10) {
+        i = "0" + i;
+    }
+    return i;
 }
