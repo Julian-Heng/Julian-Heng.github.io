1a2b43137920aa78b8044a2587078863da9b987b
Julian Heng <julianhengwl@gmail.com>
2018-03-01 00:05:24 +0800

    Updated script

--- clock.js
+++ clock.js
@@ -3,14 +3,14 @@ function startTime() {
     var h = date.getHours();
     var m = date.getMinutes();
     // add a zero in front of numbers<10
-    m = checkTime(m);
+    h = padding(h, 2);
+    m = padding(m, 2);
     document.getElementById("clock").innerHTML = h + ":" + m;
     t = setTimeout("startTime()",500);
 }
 
-function checkTime(i) {
-    if (i<10) {
-        i = "0" + i;
-    }
-    return i;
+function padding(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
