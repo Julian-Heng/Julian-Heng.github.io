a7fd6d34ba0804dabcee3f52da12cae1f9ebb47a
julian-heng <julianhengwl@gmail.com>
2019-11-04 23:27:17 +0800

    Update all

--- clock.js
+++ clock.js
@@ -1,16 +1,10 @@
-function startTime() {
-    var date = new Date();
-    var h = date.getHours();
-    var m = date.getMinutes();
-    // add a zero in front of numbers<10
-    h = padding(h, 2);
-    m = padding(m, 2);
-    document.getElementById("clock").innerHTML = h + ":" + m;
-    t = setTimeout("startTime()",500);
+function setTime()
+{
+    let date = new Date();
+    let locale = {minimumIntegerDigits: 2, useGrouping: false};
+    let hours = (date.getHours()).toLocaleString("en-AU", locale);
+    let mins = (date.getMinutes()).toLocaleString("en-AU", locale);
+    document.getElementById("clock").innerHTML = hours + ":" + mins;
 }
 
-function padding(n, width, z) {
-    z = z || '0';
-    n = n + '';
-    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-}
+setInterval(() => setTime(), 500);
